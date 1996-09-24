316a8e45348c993fbb4c2c00f3d3c4812b4c6620
Eric S. Raymond <esr@thyrsus.com>
1996-09-24 18:04:26 +0000

    Moved the protocol designation definition.
    svn path=/trunk/; revision=119

--- fetchmail.h
+++ fetchmail.h
@@ -11,7 +11,13 @@
 
  ***********************************************************************/
 
-
+/* constants designating the various supported protocols */
+#define		P_AUTO	0
+#define		P_POP2	2
+#define		P_POP3	3
+#define		P_IMAP	4
+#define		P_APOP	5
+#define		P_RPOP	6
 
 /* definitions for buffer sizes -- somewhat arbitrary */
 #define		POPBUFSIZE	512	/* per RFC 937 */
