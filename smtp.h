3f972ba1ec94c2099f6d55d1427fcce3a543dc56
Eric S. Raymond <esr@thyrsus.com>
1996-09-24 00:32:54 +0000

    Initial revision
    svn path=/trunk/; revision=113

---
+++ smtp.h
@@ -0,0 +1,34 @@
+/* Copyright 1996 by Eric S. Raymond
+ * All rights reserved.
+ * For license terms, see the file COPYING in this directory.
+ */
+
+/***********************************************************************
+  module:       smtp.h
+  project:      popclient
+  description:  Prototypes for smtp handling code.
+
+ ***********************************************************************/
+
+#ifndef _POPSMTP_
+#define _POPSMTP_
+
+#define         SMTPBUFSIZE     128  
+
+/* SMTP error values */
+#define         SM_OK              0
+#define         SM_ERROR           128
+#define         SM_UNRECOVERABLE   129
+
+#ifdef HAVE_PROTOTYPES
+int SMTP_helo(int socket,char *host);
+int SMTP_from(int socket,char *from);
+int SMTP_rcpt(int socket,char *to);
+int SMTP_data(int socket);
+int SMTP_eom(int socket);
+int SMTP_ok(int socket,char *argbuf);
+int SMTP_Gets(int socket,char *buf,int sz);
+void SMTP_rset(int socket);
+#endif /* HAVE_PROTOTYPES */
+
+#endif
