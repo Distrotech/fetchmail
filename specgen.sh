4390751f75b6cd65386576b11101e287dbb671e5
Eric S. Raymond <esr@thyrsus.com>
1996-10-31 21:46:57 +0000

    Make the RPM spec include the patchlevel.
    svn path=/trunk/; revision=460

--- specgen.sh
+++ specgen.sh
@@ -2,7 +2,7 @@ cat <<EOF
 Description: Remote mail fetcher for POP2, POP3, APOP, IMAP
 Name: fetchmail
 Version: ${1}
-Release: 1
+Release: ${2}
 Source: locke.ccil.org:/pub/esr/fetchmail-${1}.tar.gz
 Copyright: distributable
 Group: Mail
