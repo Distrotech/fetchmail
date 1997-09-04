33cb75c83198f228451e9a235f17254922ea5a1d
Eric S. Raymond <esr@thyrsus.com>
1997-09-04 02:56:23 +0000

    Portability hack for alphas.
    svn path=/trunk/; revision=1297

--- md5global.h
+++ md5global.h
@@ -22,7 +22,11 @@ typedef unsigned char *POINTER;
 typedef unsigned short int UINT2;
 
 /* UINT4 defines a four byte word */
+#if SIZEOF_INT == 4
+typedef unsigned int UINT4;
+#else
 typedef unsigned long int UINT4;
+#endif
 
 /* PROTO_LIST is defined depending on how PROTOTYPES is defined above.
 If using PROTOTYPES, then PROTO_LIST returns the list, otherwise it
