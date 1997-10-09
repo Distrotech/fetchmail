bc03bb7eec2c6b0bd3d6e7ebd9d8faba6ba08a5f
Eric S. Raymond <esr@thyrsus.com>
1997-10-09 21:26:53 +0000

    Ooops.
    svn path=/trunk/; revision=1493

--- driver.c
+++ driver.c
@@ -1543,7 +1543,7 @@ const struct method *proto;	/* protocol method table */
 
 		if (outlevel >= O_VERBOSE)
 		    if (idp->next)
-			error(0, 0, "selecting or re-polling folder %s");
+			error(0, 0, "selecting or re-polling folder %s", idp->id);
 		    else
 			error(0, 0, "selecting or re-polling default folder");
 
