493bdbd8dbfb82f9689984498abc0ccb72e3baba
Eric S. Raymond <esr@thyrsus.com>
1998-06-30 16:05:55 +0000

    Enable CompuServe users to suppress RPA.
    svn path=/trunk/; revision=1952

--- pop3.c
+++ pop3.c
@@ -119,7 +119,9 @@ int pop3_getauth(int sock, struct query *ctl, char *greeting)
 	  * CompuServe has changed its RPA behavior.  Used to be they didn't
 	  * accept PASS, but I'm told this changed in mid-November 1997.
 	  */
-	 if (strstr(greeting, "csi.com"))
+	 if (strstr(greeting, "csi.com")
+	     && (start = strchr(ctl->remotename, '@'))
+	     && !strcmp("@compuserve.com", start))
 	 {
              /* temporary fix to get back out of cleartext authentication */
              gen_transact(sock, "PASS %s", "dummypass");
