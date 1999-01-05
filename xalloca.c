f06b1846a2580c3b19a89f5f5e76b7d509afb3c9
Eric S. Raymond <esr@thyrsus.com>
1999-01-05 04:02:40 +0000

    Initial revision
    svn path=/trunk/; revision=2327

---
+++ xalloca.c
@@ -0,0 +1,47 @@
+/*
+ * xalloca.c -- allocate space or die 
+ *
+ * For license terms, see the file COPYING in this directory.
+ */
+
+#include "config.h"
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#if defined(STDC_HEADERS)
+#include  <stdlib.h>
+#endif
+#if defined(HAVE_ALLOCA_H)
+#include <alloca.h>
+#else
+#ifdef _AIX
+ #pragma alloca
+#endif
+#endif
+
+#include "fetchmail.h"
+
+#if defined(HAVE_VOIDPOINTER)
+#define XALLOCATYPE void
+#else
+#define XALLOCATYPE char
+#endif
+
+XALLOCATYPE *
+#ifdef __STDC__
+xalloca (size_t n)
+#else
+xalloca (n)
+
+int n;
+#endif
+{
+    XALLOCATYPE *p;
+
+    p = (XALLOCATYPE *) alloca(n);
+    if (p == (XALLOCATYPE *) 0)
+	error(PS_UNDEFINED, 0, "alloca failed");
+    return(p);
+}
+
+/* xalloca.c ends here */
