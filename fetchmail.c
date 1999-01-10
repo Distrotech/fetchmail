d9af45254db506bfc07b3be948f172ccd9e8159b
Eric S. Raymond <esr@thyrsus.com>
1999-01-10 00:10:42 +0000

    Make the mimedecode default TRUE.
    svn path=/trunk/; revision=2346

--- fetchmail.c
+++ fetchmail.c
@@ -944,7 +944,7 @@ static int load_params(int argc, char **argv, int optind)
 	    DEFAULT(ctl->forcecr, FALSE);
 	    DEFAULT(ctl->pass8bits, FALSE);
 	    DEFAULT(ctl->dropstatus, FALSE);
-	    DEFAULT(ctl->mimedecode, FALSE);
+	    DEFAULT(ctl->mimedecode, TRUE);
 	    DEFAULT(ctl->server.dns, TRUE);
 	    DEFAULT(ctl->server.uidl, FALSE);
 	    DEFAULT(ctl->server.checkalias, FALSE);
