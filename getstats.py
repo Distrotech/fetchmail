b1b8334afb2f046c5f3f1115974ce4e76a046cfa
Eric S. Raymond <esr@thyrsus.com>
2001-02-07 15:36:50 +0000

    Good, we can get list statistics again.
    svn path=/trunk/; revision=3032

--- getstats.py
+++ getstats.py
@@ -10,17 +10,9 @@ ln = commands.getoutput("co -p RCS/*.[chly],v 2>/dev/null | wc -l")
 vers = commands.getoutput("sed -n -e '/VERSION/s/VERSION *= *\\(.*\\)/\\1/p' <Makefile")
 print "fetchmail-" + vers + " (" + date + "), " + string.strip(ln) + " lines:"
 
-# Get mailing-list statistics.
-def bumpcount(str):
-    global linecount
-    linecount = linecount + 1
-ftp = ftplib.FTP('locke.ccil.org', 'esr', 'Malvern')
-linecount = 0
-ftp.retrlines("RETR fetchmail-friends", bumpcount)
-friends = linecount - 1
-linecount = 0
-ftp.retrlines("RETR fetchmail-announce", bumpcount)
-announce = linecount
-print "There are %d people on fetchmail-friends and %d on fetchmail-announce."% (friends, announce)
+# Use local listsize command to grab list statistics
+friends = commands.getoutput("listsize friends").strip()
+announce = commands.getoutput("listsize announce").strip()
+print "There are %s people on fetchmail-friends and %s on fetchmail-announce."% (friends, announce)
 
 # getstats.py
