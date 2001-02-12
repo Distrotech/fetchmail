b0e55a125bea3cc870d9d2fe11e20b8eb27d8a8d
Eric S. Raymond <esr@thyrsus.com>
2001-02-12 07:57:35 +0000

    This seems to work.
    svn path=/trunk/; revision=3079

--- testservers-gen.sh
+++ testservers-gen.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+date=`date`
+cat <<EOF
 <!DOCTYPE HTML public "-//W3O//DTD W3 HTML 4.0//EN">
 <HTML>
 <HEAD>
@@ -10,7 +14,7 @@
 <table width="100%" cellpadding=0><tr>
 <td width="30%">Back to <a href="/~esr">Eric's Home Page</a>
 <td width="30%" align=center>Up to <a href="/~esr/sitemap.html">Site Map</a>
-<td width="30%" align=right>$Date: 2001/02/12 05:37:36 $
+<td width="30%" align=right>${date}
 </table>
 <HR>
 <H1 ALIGN=CENTER>Fetchmail's Test List</H1>
@@ -19,38 +23,32 @@ Here are the server types on my regression-test list:<p>
 
 <table border=1 with=80% align=center>
 <tr>
-<td><strong>Server ID string:</strong></td>
-<td><strong>Extensions:</strong></td>	
-<td><strong>Type:</strong></td>
+<td><bold>Protocol & Version</bold></td>
+<td><bold>Special Options:</bold></td>
+<td><bold>Name:</bold></td>
 </tr>
-<tr>
-<td>IMAP4rev1 v12.264</td>	<td>IMAP4REV1</td>	<td>UW IMAP</td>
-</tr>
-<tr>
-<td>POP3 v7.64</td>		<td>(none)</td>		<td>Unknown</td>
-</tr>
-<tr>
-<td>QPOP (version 2.53)</td>	<td>(none)</td>		<td>Eudora QPopper</td>
-</tr>
-<tr>
-<td>IMAP4rev1 v10.168</td>	<td>IMAP4REV1</td>	<td>UW IMAP</td>
-</tr>
-<tr>
-<td>IMAP4rev1 2000.287</td>	<td>AUTH=CRAM-MD5</td>	<td>IMAP 2000</td>
-</tr>
-<tr>
-<td>POP3 v2000.70</td>		<td>AUTH=CRAM-MD5</td>	<td>IMAP 2000 POP3 gateway</td>
-</tr>
+EOF
+torturetest.py -t
+cat <<EOF
 </table>
 
+<p>
+If you control a post-office server that is not one of the types listed
+here, please consider lending me a test account.  Note that I do <em>not</em>
+need shell access, just the permissions to send mail to a mailbox the server
+looks at and to fetch mail off of it.<P>
+<p>
+I'd like to have weird things like a POP2 server and broken things like
+Microsoft Exchange on here. These are the real robustness tests.
 <HR>
 <table width="100%" cellpadding=0><tr>
 <td width="30%">Back to <a href="/~esr">Eric's Home Page</a>
 <td width="30%" align=center>Up to <a href="/~esr/sitemap.html">Site Map</a>
-<td width="30%" align=right>$Date: 2001/02/12 05:37:36 $
+<td width="30%" align=right>${dateQ}
 </table>
 
 <P><ADDRESS>Eric S. Raymond <A HREF="mailto:esr@thyrsus.com">&lt;esr@thyrsus.com&gt;</A></ADDRESS>
 </BODY>
 </HTML>
+EOF
 
