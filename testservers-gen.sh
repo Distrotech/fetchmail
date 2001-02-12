2a166a2abb00691c386135158d9f6f3a8dbaa0f1
Eric S. Raymond <esr@thyrsus.com>
2001-02-12 05:37:36 +0000

    Initial revision
    svn path=/trunk/; revision=3078

---
+++ testservers-gen.sh
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML public "-//W3O//DTD W3 HTML 4.0//EN">
+<HTML>
+<HEAD>
+<link rev=made href="mailto:esr@snark.thyrsus.com">
+<meta name="description" content="">
+<meta name="keywords" content=""> 
+<TITLE>Fetchmail's Test List</TITLE>
+</HEAD>
+<BODY>
+<table width="100%" cellpadding=0><tr>
+<td width="30%">Back to <a href="/~esr">Eric's Home Page</a>
+<td width="30%" align=center>Up to <a href="/~esr/sitemap.html">Site Map</a>
+<td width="30%" align=right>$Date: 2001/02/12 05:37:36 $
+</table>
+<HR>
+<H1 ALIGN=CENTER>Fetchmail's Test List</H1>
+
+Here are the server types on my regression-test list:<p>
+
+<table border=1 with=80% align=center>
+<tr>
+<td><strong>Server ID string:</strong></td>
+<td><strong>Extensions:</strong></td>	
+<td><strong>Type:</strong></td>
+</tr>
+<tr>
+<td>IMAP4rev1 v12.264</td>	<td>IMAP4REV1</td>	<td>UW IMAP</td>
+</tr>
+<tr>
+<td>POP3 v7.64</td>		<td>(none)</td>		<td>Unknown</td>
+</tr>
+<tr>
+<td>QPOP (version 2.53)</td>	<td>(none)</td>		<td>Eudora QPopper</td>
+</tr>
+<tr>
+<td>IMAP4rev1 v10.168</td>	<td>IMAP4REV1</td>	<td>UW IMAP</td>
+</tr>
+<tr>
+<td>IMAP4rev1 2000.287</td>	<td>AUTH=CRAM-MD5</td>	<td>IMAP 2000</td>
+</tr>
+<tr>
+<td>POP3 v2000.70</td>		<td>AUTH=CRAM-MD5</td>	<td>IMAP 2000 POP3 gateway</td>
+</tr>
+</table>
+
+<HR>
+<table width="100%" cellpadding=0><tr>
+<td width="30%">Back to <a href="/~esr">Eric's Home Page</a>
+<td width="30%" align=center>Up to <a href="/~esr/sitemap.html">Site Map</a>
+<td width="30%" align=right>$Date: 2001/02/12 05:37:36 $
+</table>
+
+<P><ADDRESS>Eric S. Raymond <A HREF="mailto:esr@thyrsus.com">&lt;esr@thyrsus.com&gt;</A></ADDRESS>
+</BODY>
+</HTML>
+
