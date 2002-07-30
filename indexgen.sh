1b7d5ca91158672a79aed729d3a0a5714b28553d
Eric S. Raymond <esr@thyrsus.com>
2002-07-30 13:31:23 +0000

    XML headers everywhere.
    svn path=/trunk/; revision=3682

--- indexgen.sh
+++ indexgen.sh
@@ -41,6 +41,7 @@ gpg --detach-sign --armor fetchmail-$version.tar.gz
 EOF
 
 cat >index.html <<EOF
+<?xml version="1.0" encoding="ISO-8859-1"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
