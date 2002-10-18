c50c2468319feafec54a124fac707f24979fb5ac
Eric S. Raymond <esr@thyrsus.com>
2002-10-18 19:34:20 +0000

    Ready to ship.
    svn path=/trunk/; revision=3752

--- indexgen.sh
+++ indexgen.sh
@@ -3,7 +3,7 @@
 # indexgen.sh -- generate current version of fetchmail home page.
 #
 goldvers="6.1.0"
-goldname="6.0.0"
+goldname="6.1.0"
 version=`sed -n <Makefile.in "/VERSION *= */s/VERSION *= *\([^ 	]*\)/\1/p"`
 date=`date "+%d %b %Y"`
 
