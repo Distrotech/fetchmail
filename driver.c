be98bd8f36415ba5a569a21c38f5aed8eef932df
Eric S. Raymond <esr@thyrsus.com>
2004-01-12 17:15:23 +0000

    Fetchseizelimit must be 1 for all POP-family protocols.
    svn path=/trunk/; revision=3863

--- driver.c
+++ driver.c
@@ -429,8 +429,11 @@ static int fetch_messages(int mailserver_socket, struct query *ctl,
 	/* for POP3, we can get the size of one mail only! Unfortunately, this
 	 * protocol specific test cannot be done elsewhere as the protocol
 	 * could be "auto". */
-	if (ctl->server.protocol == P_POP3)
+	switch (ctl->server.protocol)
+	{
+	    case P_POP3: case P_APOP: case P_RPOP:
 	    fetchsizelimit = 1;
+	}
 
 	/* Time to allocate memory to store the sizes */
 	xalloca(msgsizes, int *, sizeof(int) * fetchsizelimit);
