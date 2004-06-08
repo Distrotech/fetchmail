bcaf7adb741958daf53a16ed7991ad88e44df695
nobody <nobody@localhost>
2004-06-08 04:03:10 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_1-9-7'.
    svn path=/tags/RELEASE_1-9-7/; revision=494

--- socket.h
+++ socket.h
@@ -9,12 +9,20 @@
 #ifndef SOCKET__
 #define SOCKET__
 
+#ifndef  INADDR_NONE
+#ifdef   INADDR_BROADCAST
+#define  INADDR_NONE	INADDR_BROADCAST
+#else
+#define	 INADDR_NONE	-1
+#endif
+#endif
+
 #if defined(HAVE_PROTOTYPES)
 /*
 Create a new client socket 
-returns (FILE *)NULL on error 
+returns < 0 on error 
 */
-FILE *Socket(char *host, int clientPort);
+int Socket(char *host, int clientPort);
 
 /* 
 Get a string terminated by an '\n', delete any '\r' and the '\n'.
@@ -22,13 +30,25 @@ Pass it a valid socket, a buffer for the string, and
 the length of the buffer (including the trailing \0)
 returns 0 for success. 
 */
-int SockGets(char *buf, int len, FILE *sockfp);
+int SockGets(int socket, char *buf, int len);
+
+/*
+Send a nul terminated string to the socket, followed by 
+a CR-LF.  Returns 0 for success.
+*/
+int SockPuts(int socket, char *buf);
 
 /*
 Write a chunk of bytes to the socket.
 Returns 0 for success.
 */
-int SockWrite(char *buf, int len, FILE *sockfp);
+int SockWrite(int socket, char *buf, int len);
+
+/*
+Read a chunk of bytes from the socket.
+Returns 0 for success.
+*/
+int SockRead(int socket, char *buf, int len);
 
 /* 
 Send formatted output to the socket, followed
@@ -36,10 +56,15 @@ by a CR-LF.
 Returns 0 for success.
 */
 #if defined(HAVE_STDARG_H)
-int SockPrintf(FILE *sockfp, char *format, ...) ;
+int SockPrintf(int socket, char *format, ...) ;
 #else
 int SockPrintf();
 #endif
+/*
+Check socket for readability.  return 0 for not readable,
+>0 for readable.
+*/
+int SockStatus(int socket, int seconds);
  
 #endif /* defined(HAVE_PROTOTYPES) */
 
