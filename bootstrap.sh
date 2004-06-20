85c93ddaca632144b14667e1c66aa14436d1a8f3
Matthias Andree <matthias.andree@gmx.de>
2004-06-20 09:28:56 +0000

    Remove old autogen.sh.
    svn path=/trunk/; revision=3910

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,10 +10,28 @@ test -f fetchmail.c
 test -f Makefile.am
 rm -rf autom4te.cache
 echo
-echo "Please stand by while generating files with autoreconf, this may"
-echo "take a minute or two..."
+echo "Please stand by while generating files,"
+echo "this may take a minute or two..."
 echo
-autoreconf -isv
+# Original autogen.sh:
+#rm -f po/Makefile.in.in po/ChangeLog po/ChangeLog~ || true
+#gettextize -c -f || true
+
+# do not use -s here, Eric S. Raymond (ESR) writes they don't
+# work well in Debian's build system
+autoreconf -iv
+
+# Taken from ESR's autgen.sh:
+#
+# For the Debian build, refresh list of +x scripts, to avoid
+# possible breakage if upstream tarball does not include the file
+# or if it is mispackaged for whatever reason
+#
+test -d debian && {
+	rm -f debian/executable.files
+	find -type f -perm +111 ! -name '.*' -fprint debian/executable.files
+}
+
 echo
 echo "You can now run ./configure and make as usual. See INSTALL for details."
 echo
