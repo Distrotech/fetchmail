29199c08cd359ef2ef24eb08f1f983c6ba76e4db
Matthias Andree <matthias.andree@gmx.de>
2004-11-10 00:25:46 +0000

    Add help for --sslcertpath and --sslfingerprint.
    svn path=/trunk/; revision=3990

--- options.c
+++ options.c
@@ -683,6 +683,8 @@ struct query *ctl;	/* option record to be initialized */
 	P(GT_("      --ssl         enable ssl encrypted session\n"));
 	P(GT_("      --sslkey      ssl private key file\n"));
 	P(GT_("      --sslcert     ssl client certificate\n"));
+	P(GT_("      --sslcertpath path to ssl certificates\n"));
+	P(GT_("      --sslfingerprint fingerprint that must match that of the server's cert.\n"));
 	P(GT_("      --sslproto    force ssl protocol (ssl2/ssl3/tls1)\n"));
 #endif
 	P(GT_("      --plugin      specify external command to open connection\n"));
