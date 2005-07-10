97a4f74adfc1b26a065253aae2a9190385a6c3f8
Matthias Andree <matthias.andree@gmx.de>
2005-07-10 22:13:39 +0000

    Skip list subscriber counts. Check for SVN errors.
    svn path=/trunk/; revision=4108

--- dist-tools/getstats.py
+++ dist-tools/getstats.py
@@ -9,7 +9,9 @@ date = commands.getoutput("LC_TIME=C date -u")
 pid = os.getpid()
 # this is a fast variant using the base of the current working directory
 # (ignores uncommitted modifications)
-os.system("svn export -rBASE . /tmp/getstats.%d" % pid)
+if os.system("svn export -q -rBASE . /tmp/getstats.%d" % pid):
+    print "SVN FAILED"
+    os.exit(1)
 # this is a slower variant that may export the whole tree across the net
 #os.system("svn export -rCOMMITTED . /tmp/getstats.%d" % pid)
 ln = commands.getoutput("cat /tmp/getstats.%d/*.[chly] 2>/dev/null | wc -l" % pid)
@@ -18,8 +20,8 @@ vers = commands.getoutput("sed -n -e '/AC_INIT/s/AC_INIT(\[.*\],\[\\(.*\\)\])/\\
 print "fetchmail-" + vers + " (" + date + "), " + string.strip(ln) + " lines:"
 
 # Use local listsize command to grab list statistics
-friends = commands.getoutput("listsize friends").strip()
-announce = commands.getoutput("listsize announce").strip()
-print "There are %s people on fetchmail-friends and %s on fetchmail-announce."% (friends, announce)
+#friends = commands.getoutput("listsize friends").strip()
+#announce = commands.getoutput("listsize announce").strip()
+#print "There are %s people on fetchmail-friends and %s on fetchmail-announce."% (friends, announce)
 
 # getstats.py
