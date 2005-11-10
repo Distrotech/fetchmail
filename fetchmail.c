Distrotech/fetchmail

commit bcc358c34513559116ad49673029eddcbf7a5bf0

Use new host_fqdn() interface.
svn path=/trunk/; revision=4382

fetchmail.c
@@ -1076,9 +1076,12 @@ static int load_params(int argc, char **argv, int optind)
 		 || ctl->server.authenticate == A_KERBEROS_V4
 		 || ctl->server.authenticate == A_KERBEROS_V5))
 	{
-	    fetchmailhost = host_fqdn();
+	    fetchmailhost = host_fqdn(1);
 	    break;
 	}
+
+    if (!ctl) /* list exhausted */
+	fetchmailhost = host_fqdn(0);
 
     /* this code enables flags to be turned off */
 #define DEFAULT(flag, dflt)	if (flag == FLAG_TRUE)\

sink.c
@@ -293,10 +293,13 @@ static int send_bouncemail(struct query *ctl, struct msgblk *msg,
 
     /* can't just use fetchmailhost here, it might be localhost */
     if (fqdn_of_host == NULL)
-	fqdn_of_host = host_fqdn();
+	fqdn_of_host = host_fqdn(0); /* can't afford to bail out and
+					lose the NDN here */
     strlcat(daemon_name, fqdn_of_host, sizeof(daemon_name));
 
     /* we need only SMTP for this purpose */
+    /* XXX FIXME: hardcoding localhost is nonsense if smtphost can be
+     * configured */
     if ((sock = SockOpen("localhost", SMTP_PORT, NULL)) == -1)
 	return(FALSE);
 

commit 0456a78a265f6eef6607a42bf5afef22178cd5a0

Unbreak --smtphost default, reported and different patch suggested by Sunil Shetye. Broken since r4382 v6.2.9-rc7.
svn path=/trunk/; revision=4402

fetchmail.c
@@ -922,7 +922,7 @@ static void optmerge(struct query *h2, struct query *h1, int force)
  *         - false if servers found on the command line */
 static int load_params(int argc, char **argv, int optind)
 {
-    int	implicitmode, st;
+    int	implicitmode, st, use_kerberos;
     struct passwd *pw;
     struct query def_opts, *ctl;
     struct stat rcstat;
@@ -1070,18 +1070,20 @@ static int load_params(int argc, char **argv, int optind)
      * If we're using Kerberos for authentication, we need 
      * the FQDN in order to generate capability keys.
      */
+    use_kerberos = 0;
+    /* use_kerberos is saved until later to properly set the invisible
+     * --smtphost (aka. smtphunt) default */
     for (ctl = querylist; ctl; ctl = ctl->next)
 	if (ctl->active && 
 		(ctl->server.protocol==P_ETRN || ctl->server.protocol==P_ODMR
 		 || ctl->server.authenticate == A_KERBEROS_V4
 		 || ctl->server.authenticate == A_KERBEROS_V5))
 	{
-	    fetchmailhost = host_fqdn(1);
+	    use_kerberos = 1;
 	    break;
 	}
 
-    if (!ctl) /* list exhausted */
-	fetchmailhost = host_fqdn(0);
+    fetchmailhost = host_fqdn(use_kerberos);
 
     /* this code enables flags to be turned off */
 #define DEFAULT(flag, dflt)	if (flag == FLAG_TRUE)\
@@ -1156,7 +1158,7 @@ static int load_params(int argc, char **argv, int optind)
 	     * Make sure we have a nonempty host list to forward to.
 	     */
 	    if (!ctl->smtphunt)
-		save_str(&ctl->smtphunt, fetchmailhost, FALSE);
+		save_str(&ctl->smtphunt, use_kerberos ? fetchmailhost : "localhost", FALSE);
 
 	    /*
 	     * Make sure we have a nonempty list of domains to fetch from.