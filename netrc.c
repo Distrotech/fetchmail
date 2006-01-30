f6a2380b73de5b9c5fe306f362c8f5597749c552
Matthias Andree <matthias.andree@gmx.de>
2006-01-30 09:00:03 +0000

    Craig Leres: only overwrite password if the pointer is non-NULL. Avoids a crash in .netrc files with missing passwords.
    svn path=/branches/BRANCH_6-3/; revision=4684

--- netrc.c
+++ netrc.c
@@ -314,8 +314,10 @@ void
 free_netrc(netrc_entry *a) {
     while(a) {
 	netrc_entry *n = a->next;
-	memset(a->password, 0x55, strlen(a->password));
-	xfree(a->password);
+	if (a->password != NULL) {
+		memset(a->password, 0x55, strlen(a->password));
+		free(a->password);
+	}
 	xfree(a->login);
 	xfree(a->host);
 	xfree(a);
