4de85d854ded68df6bce92c67d4f6cc25a3fe414
Matthias Andree <matthias.andree@gmx.de>
2009-08-06 00:18:41 +0000

    Update.
    svn path=/branches/BRANCH_6-3/; revision=5401

--- dist-tools/send-security-announce.sh
+++ dist-tools/send-security-announce.sh
@@ -1,16 +1,15 @@
 test "$SEND_ANNOUNCEMENTS" = doit || { echo "read the source, luke! -> $0" ; cat "$0" ; exit 1 ; }
 
-ANN=fetchmail-SA-2008-02
-FILE=$HOME/mywork/fetchmail/BRANCH_6-3/$ANN.txt
+ANN=fetchmail-SA-2009-01
+FILE=$HOME/VCS-mine/fetchmail.svn/BRANCH_6-3/$ANN.txt
 test -r $FILE || { echo "Cannot find $FILE." ; exit 1 ; }
 CVE=$(sed -n '/^CVE Name:/ { s/^.*:[ 	]*//p;q; }' $FILE)
 SUBJECT="fetchmail security announcement $ANN ($CVE)"
+MAILER=mail
 
-nail -r ma+nomail@dt.e-technik.uni-dortmund.de -s "$SUBJECT" <$FILE \
-    vulnwatch@vulnwatch.org
-
-nail -r ma+bt@dt.e-technik.uni-dortmund.de -s "$SUBJECT" <$FILE \
+$MAILER -r ma+bt@dt.e-technik.uni-dortmund.de -s "$SUBJECT" <$FILE \
     bugtraq@securityfocus.com
 
-nail -r matthias.andree@gmx.de -s "$SUBJECT" <$FILE \
-    vendor-sec@lst.de fetchmail-announce@lists.berlios.de
+$MAILER -r matthias.andree@gmx.de -s "$SUBJECT" <$FILE \
+    vendor-sec@lst.de \
+    fetchmail-announce@lists.berlios.de
