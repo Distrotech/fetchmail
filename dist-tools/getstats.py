c486e64596db92a4319f48f27832fd7ff8cc639d
Matthias Andree <matthias.andree@gmx.de>
2009-10-30 02:23:23 +0000

    Clean up dist-tools, remove or update.
    svn path=/branches/BRANCH_6-3/; revision=5446

--- dist-tools/getstats.py
+++ dist-tools/getstats.py
@@ -7,21 +7,22 @@ import commands, os, string, ftplib
 # Get version and date
 date = commands.getoutput("LC_TIME=C date -u")
 pid = os.getpid()
-# this is a fast variant using the base of the current working directory
-# (ignores uncommitted modifications)
-if os.system("svn export -q -rBASE . /tmp/getstats.%d" % pid):
+if True:
+    # this is a fast variant using the base of the current working directory
+    # (ignores uncommitted modifications)
+    cmd = "svn export -q -rBASE . /tmp/getstats.%d" % pid
+else:
+    #  this is a slower variant that may export the whole tree across
+    #  the net
+    cmd = "svn export -rCOMMITTED . /tmp/getstats.%d" % pid
+
+if os.system(cmd):
     print "SVN FAILED"
     os.exit(1)
-# this is a slower variant that may export the whole tree across the net
-#os.system("svn export -rCOMMITTED . /tmp/getstats.%d" % pid)
+
 ln = commands.getoutput("cat /tmp/getstats.%d/*.[chly] 2>/dev/null | wc -l" % pid)
 os.system("rm -rf /tmp/getstats.%d" % pid)
-vers = commands.getoutput("sed -n -e '/AC_INIT/s/AC_INIT(\[.*\],\[\\(.*\\)\])/\\1/p' <configure.ac")
+vers = commands.getoutput("sed -n -e '/AC_INIT/s/AC_INIT(\\[.\\+\\],\\[\\(.*\\)\\],.*)/\\1/p' <configure.ac")
 print "fetchmail-" + vers + " (" + date + "), " + string.strip(ln) + " lines:"
 
-# Use local listsize command to grab list statistics
-#friends = commands.getoutput("listsize friends").strip()
-#announce = commands.getoutput("listsize announce").strip()
-#print "There are %s people on fetchmail-friends and %s on fetchmail-announce."% (friends, announce)
-
-# getstats.py
+# end of getstats.py
