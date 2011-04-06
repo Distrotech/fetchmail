70d07b87092ff981ee5b72d4ecdf278f978bcf02
Matthias Andree <matthias.andree@gmx.de>
2011-04-06 21:25:33 +0200

    Make test less verbose

--- t_IMAPCapa.cpp
+++ t_IMAPCapa.cpp
@@ -28,9 +28,10 @@ static bool t_matchbaricase(const IMAPCapa &c) { return c["bAr"]; }
 
 static bool t_notfound(const IMAPCapa &c) { return !c["NoNeXiSt"]; }
 
-static bool check(const string &name, bool (*func)(const IMAPCapa &c), const IMAPCapa &c) {
+static bool check(const string &name, bool (*func)(const IMAPCapa &c), const IMAPCapa &c, bool verbose) {
     bool result = func(c);
-    cout << (result ? "- PASS: " : "- FAIL: ") << name << "\n";
+    if (verbose)
+	cout << (result ? "- PASS: " : "- FAIL: ") << name << "\n";
     return result;
 }
 
@@ -38,17 +39,19 @@ int main() {
     IMAPCapa c;
     string s("FOO BaR\tBAZ");
     bool result = true;
+    bool verbose = getenv("TEST_VERBOSE");
 
-    result &= check("if uninitialized access throws exception", t_throwifuninit, c);
+    result &= check("if uninitialized access throws exception", t_throwifuninit, c, verbose);
     c.parse("");
-    result &= check("if initialized access does not throw exception", t_nothrowifinit, c);
+    result &= check("if initialized access does not throw exception", t_nothrowifinit, c, verbose);
     c.parse(s);
-    cout << "- TEST: parsing \"" << s << "\" yields \"" << c.str() << "\"\n";
-    result &= check("if we match sensitively", t_matchfoocase, c);
-    result &= check("if we match insensitively", t_matchbaricase, c);
-    result &= check("if we handle not-found properly", t_notfound, c);
+    if (verbose)
+	cout << "- TEST: parsing \"" << s << "\" yields \"" << c.str() << "\"\n";
+    result &= check("if we match sensitively", t_matchfoocase, c, verbose);
+    result &= check("if we match insensitively", t_matchbaricase, c, verbose);
+    result &= check("if we handle not-found properly", t_notfound, c, verbose);
     c.flush();
-    result &= check("if uninitialized access throws exception", t_throwifuninit, c);
+    result &= check("if uninitialized access throws exception", t_throwifuninit, c, verbose);
 
     return !result;
 }
